53ac9f00c8e655014a73e0a1efeb0468431d6b16
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-03-06 10:30:32 +0200

    Update frontend side

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
+import { Nav, Navbar, Container } from "react-bootstrap";
+
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Navbar variant="dark">
+        <Container>
+          <Navbar.Brand
+            style={{ color: "#646464", fontWeight: "500" }}
+            href="#home"
+          >
+            MERN App
+          </Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link
+              style={{ color: "#646464" }}
+              className="mr-2"
+              href="#home"
+            >
+              Posts
+            </Nav.Link>
+            <Nav.Link
+              style={{ color: "#646464" }}
+              className="mr-2"
+              href="#707070tures"
+            >
+              Submit Post
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
